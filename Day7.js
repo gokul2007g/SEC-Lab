d052e522df24138a798be5b08ee630ca3837fb95
gokul_05 <gokulpuvi5727@gmail.com>
2025-09-18 13:31:05 +0530

    Create Day7.js

---
+++ Day7.js
@@ -0,0 +1,34 @@
+
+let current = new Date("2024-08-08");
+let day = current.getDay();
+console.log(current)//unit
+console.log(current.toString());
+console.log(day);
+
+class Sample{
+    constructor(personname){
+        this.personname=personname
+        console.log(personname)
+    }
+}
+let result=new Sameple("js");
+
+class bank{
+    constructor(initialamount=0){
+        this._balance=initialamount;
+    }
+    get balance(){
+        return this._balance;
+    }
+    set balance(amount){
+        if(amount>0){
+            this._balance += amount;
+            console.log(amount);
+        }
+    }
+}
+let b=new bank(100);
+console.log(b)
+
+b.balance=100;
+console.log(b)
